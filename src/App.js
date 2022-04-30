ca1dced0a1fadcf22d7dfa18e5648358d43dcf3a
David Collins <dacollins3@gmail.com>
2022-04-30 15:37:41 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -7,22 +7,46 @@ import ImageGallery from 'react-image-gallery'
 
 function Body()  {
   const imageGalleryRef = useRef(null)
-  const [thumbnailClicked, setThumbnailClicked ] = useState(false)
-
-  useEffect(() => {
-    if(thumbnailClicked) {
-      imageGalleryRef.current.scrollIntoView({alignToTop:true})
-      setThumbnailClicked(false)
-    }
-  },[thumbnailClicked])
-
+  const contactRef = useRef(null)
+  const [photoClicked, setPhotoClicked ] = useState(false)
 
   const scrollToImageGallery = () => {
     imageGalleryRef.current.scrollIntoView({alignToTop:true})
   }
 
+  const onPhotoClick = () => {
+    setPhotoClicked(true)
+  }
+
+  const scrollToContact = () => {
+    contactRef.current.scrollIntoView({alignToTop:true})
+    console.log(contactRef.current)
+  }
+
+  useEffect(() => {
+    //need to wait until a re-render/for the new image to load before scrolling
+    if(photoClicked) {
+      imageGalleryRef.current.scrollIntoView({alignToTop:true})
+      setPhotoClicked(false)
+    }
+  },[photoClicked])
+
+
   return (
     <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className='container-fluid'>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <button className="btn btn-link nav-link active" onClick={scrollToImageGallery}>Photos</button>
+            </li>
+            <li className="nav-item">
+              <button className="btn btn-link nav-link active" onClick={scrollToContact}>Contact</button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      
       
       <Section {...Intro} image={SparkyLion} imageDisplay="left"/>
       <Section {...StrangerDanger}/>
@@ -33,24 +57,36 @@ function Body()  {
       <Section {...Conclusion} />
 
       
-    <div className="row mt-9" ref={imageGalleryRef}>
-    <ImageGallery items={AllImages} thumbnailPosition={'top'} onThumbnailClick={() => {setThumbnailClicked(true)}} />
-    </div>
+      <div className="row mt-9" ref={contactRef}>
+        <Contact />
+      </div>
+      
+      <div className="row mt-9" ref={imageGalleryRef}>
+        <ImageGallery items={AllImages} thumbnailPosition={'top'} onThumbnailClick={onPhotoClick} onSlide={onPhotoClick} />
+      </div>
+
+    
   </div>
   )
 }
 
+function Contact() {
+
+  return (
+    <div className='col'>
+      <h1>Contact</h1>
+      <p>Email: <a href='mailto:dacollins3@gmail.com'>dacollins3@gmail.com</a></p>
+      <p>Phone: <a href='tel:207-239-6006'>207-239-6006</a></p>
+    </div>
+  )
+  
+}
 
 function App() {
 
   return (
     <div className="App">
-      <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        navv
-      </nav>
       <Body/>
-      </div>
     </div>
   );
 }
