df0a02ad466e429c92fd783078262ab3632d7294
David Collins <dacollins3@gmail.com>
2022-05-04 21:24:44 -0400

    Updating section text

--- src/section-text/intro.js
+++ src/section-text/intro.js
@@ -1,9 +1,5 @@
 const Intro = {
-    value:  `Sparky loves his humans. He will nearly insist on being in the same room and ideally on someone's lap. If allowed, he would be cuddled up with you at night [he does sleep in his crate but we let him out for morning cuddles after the first alarm] and for every nap. He Loves camping since he's allowed to sleep in the tandem sleeping bag.
-
-Sparky loves playing with his toys and can be found some evenings running while whipping around his bear, cupcake, football, or whatever the latest toy he received from grandma. He enjoys a good game of fetch as well! (Just let him or have him drop it for you first). He also enjoys play time and horsing around with people he's comfortable with but will settle for tackling the bed sheets as his owners are attempting to change them with him on them. Sparky enjoys running around in a yard and walks but will start getting tired after a few miles. 
-
-At the end of the day, his all-time favorite activity is being with the people he loves. Sparky is a little shadow and can be found in a near-by basket, on a lap, or next to you on a couch. Sparky’s favorite evening activity is movie night being nestled between his owners.`,
+    value:  `Sparky is an older dog who likes to relax with his favorite humans! He plays with his toys on his own, and enjoys a good game of fetch as well (Just let him drop the toy before you pick it up). Sparky enjoys walks but will start getting tired after a few miles. At the end of the day, his all-time favorite activity is being with the people he loves. Sparky is a little shadow and can be found napping in a near-by basket, on a lap, or next to you on the couch. His favorite evening activity is movie night being nestled between his owners.`,
 
     "label": "intro",
     "displayName": "A little about me"
--- src/section-text/stranger-danger.js
+++ src/section-text/stranger-danger.js
@@ -1,5 +1,8 @@
 const StrangerDanger = {
-    "value": `While Sparky brings ample entertainment and joy, he comes from an unknown haunted past as a rescue and has some behavior issues. Sparky loves his family unit but has stranger danger, especially when people come over. Generally when we have company over, we keep him in his preferred upstairs room for the sake of himself and company. For extended company or people he sees regularly, we do let him out with his muzzle on. He’s fine with people on walks outside but you will have to get used to telling people no, you can’t pat him. Be extra cautious around people who are self-proclaimed ‘good with dogs’ who want to pat him (aka people who only have experience with dogs without behavioral issues).`,
+    "value": `While Sparky brings ample entertainment and joy, he comes from an unknown haunted past as a rescue and has some behavior issues. Sparky loves his family unit but has stranger danger, especially when people come over to his “home turf”. Generally when we have company over, we keep him away in his preferred upstairs room for the sake of himself and company. For extended company or people he sees regularly, we do let him out with his muzzle on. Self-proclaimed “dog people” often try to get too friendly too quickly with him, so we instruct visitors to give him space and not too much attention.
+    
+    He’s fine with strangers on walks outside but he counts on you to not let strangers invade his personal space— say a clear “no” to folks wanting to pat him. He should not spend any time with children. 
+    `,
     "label": "stranger-danger",
     "displayName": "Stranger Danger"
 }
